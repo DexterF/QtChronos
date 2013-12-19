fadf06c1c18e8add064329948c11ee7eba7c73bc
Miguel Almeida <migaxmoitax@gmail.com>
2013-12-19 01:52:30 +0000

    Simple tabed UI design with a button on the top left to be used instead of the menubar

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,11 +1,17 @@
 #include "headers/mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QPushButton>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // Create and set the options button on the top left of the tabs
+    QPushButton* buttonOptions = new QPushButton();
+    ui->tabWidget->setCornerWidget(buttonOptions, Qt::TopLeftCorner);
 }
 
 MainWindow::~MainWindow()
